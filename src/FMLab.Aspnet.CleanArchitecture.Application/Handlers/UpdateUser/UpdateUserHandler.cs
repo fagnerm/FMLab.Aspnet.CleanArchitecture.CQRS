1acddcf22e56f694d0e871f9b457f0e5227abcc4
Fagnerm <fagnermarinho.dev@gmail.com>
2026-02-26 20:48:02 -0300

    Remove Validation from Update endpoint, since user can patch only data that will be updated.

--- src/FMLab.Aspnet.CleanArchitecture.Application/Handlers/UpdateUser/UpdateUserHandler.cs
+++ src/FMLab.Aspnet.CleanArchitecture.Application/Handlers/UpdateUser/UpdateUserHandler.cs
@@ -13,19 +13,14 @@ namespace FMLab.Aspnet.CleanArchitecture.Application.Handlers.UpdateUser;
 public class UpdateUserHandler : IRequestHandler<UpdateUserCommand, Result>
 {
     private readonly IUserRepository _repository;
-    private readonly IValidator<UpdateUserCommand> _validator;
 
-    public UpdateUserHandler(IUserRepository repository, IValidator<UpdateUserCommand> validator)
+    public UpdateUserHandler(IUserRepository repository)
     {
         _repository = repository;
-        _validator = validator;
     }
 
     public async Task<Result> Handle(UpdateUserCommand input, CancellationToken cancellationToken)
     {
-        var validation = _validator.Validate(input);
-        if (!validation.IsValid)
-            return Result.Validation(error: validation.Errors[0].ErrorMessage);
 
         var user = await _repository.GetByIdAsync(input.Id, cancellationToken);
 
