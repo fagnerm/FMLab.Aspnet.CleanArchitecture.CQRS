8b46fa82ab0a83e707338ce673f559b4ff5fb913
Fagnerm <fagnermarinho.dev@gmail.com>
2026-03-04 13:51:30 -0300

    Move common DTOs to Share folder.

--- src/FMLab.Aspnet.CleanArchitecture.Application/Shared/DTOs/UserSummaryDTO.cs
+++ src/FMLab.Aspnet.CleanArchitecture.Application/Shared/DTOs/UserSummaryDTO.cs
@@ -2,7 +2,7 @@
 // Copyright (c) 2026 Fagner Marinho 
 // Licensed under the MIT License. See LICENSE file in the project root for details.
 
-namespace FMLab.Aspnet.CleanArchitecture.Application.DTOs;
+namespace FMLab.Aspnet.CleanArchitecture.Application.Shared.DTOs;
 
 public record UserSummaryDTO(
     int Id,
