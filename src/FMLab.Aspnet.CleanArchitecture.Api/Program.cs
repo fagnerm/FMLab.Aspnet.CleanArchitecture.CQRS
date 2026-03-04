286e9eae6c6d96227907b4642a4a318f0106d289
Fagnerm <fagnermarinho.dev@gmail.com>
2026-03-04 01:04:29 -0300

    Add ApiKey.

--- src/FMLab.Aspnet.CleanArchitecture.Api/Program.cs
+++ src/FMLab.Aspnet.CleanArchitecture.Api/Program.cs
@@ -3,8 +3,10 @@
 // Licensed under the MIT License. See LICENSE file in the project root for details.
 
 using FMLab.Aspnet.CleanArchitecture.Api.Configurations;
+using FMLab.Aspnet.CleanArchitecture.Api.Middlewares;
 using FMLab.Aspnet.CleanArchitecture.Application.DependencyInjection;
 using FMLab.Aspnet.CleanArchitecture.Infrastructure.DependencyInjection;
+using Microsoft.AspNetCore.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,12 +15,17 @@ builder.Services.AddApplication();
 builder.Services.AddAppProblemDetails();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddAppSwagger();
+builder.Services.AddAuthentication("ApiKey")
+                .AddScheme<AuthenticationSchemeOptions, ApiKeyAuthHandler>("ApiKey", null);
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
 app.UseAppSwagger();
 app.UseApplicationEndpoints();
 app.UseAppProblemDetails();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
 
